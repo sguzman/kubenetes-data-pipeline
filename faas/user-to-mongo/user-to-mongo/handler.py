31eb91ce3c76e2771933143afd36223085537c8b
Salvador Guzman <guzmansalv@gmail.com>
2021-04-28 21:18:17 -0700

    done

--- faas/user-to-mongo/user-to-mongo/handler.py
+++ faas/user-to-mongo/user-to-mongo/handler.py
@@ -1,7 +1,13 @@
+import pymongo
+
 def handle(req):
     """handle a request to the function
     Args:
         req (str): request body
     """
+
+    db = pymongo.MongoClient(host='redis-master.default.svc.cluster.local', port=27017)
+    db.duolingo.dumps.insert_one(req)
+    db.close()
 
     return req
