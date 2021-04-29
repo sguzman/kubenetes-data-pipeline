4a9476c8ff79415be841d50d0bc687fc4dc9cc14
Salvador Guzman <guzmansalv@gmail.com>
2021-04-28 22:03:41 -0700

    set auth

--- faas/user-to-mongo/user-to-mongo/handler.py
+++ faas/user-to-mongo/user-to-mongo/handler.py
@@ -9,7 +9,9 @@ def handle(req):
 
     db = pymongo.MongoClient(
         host='mongodb.default.svc.cluster.local',
-        port=27017
+        port=27017,
+        username='admin',
+        passwword='admin'
         )
     obj = json.loads(req)
     db.duolingo.dumps.insert_one(obj)
